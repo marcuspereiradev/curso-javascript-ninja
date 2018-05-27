4f2586d2496fa3cd0d9cab7b721e2d32bd1c3fd0
marcuspereiragithub <vinnycoast@gmail.com>
2018-05-27 16:53:04 -0300

    challenge-22

--- challenge-22/challenge-22.js
+++ challenge-22/challenge-22.js
@@ -38,11 +38,12 @@
     Na primeira linha, dentro da função, deixe um console.log para mostrar todos
     os parâmetros passados para essa função.
     */
+    
     function sum() {
         console.log(arguments);
         var result = Array.prototype.reduce.call(arguments, function(acumulado, atual){
             return acumulado + atual;
-        });
+        },0);
         console.log(result);
     }
     sum(1,2,3,4,5,6,7,8,9,10,11);
@@ -52,20 +53,22 @@
     diferentes, com quantidades variáveis de parâmetros passados.
     */
     console.log( '\nSomar alguns números:' );
-    console.log(sum(2,3).call(result));
-
+    console.log(sum(2,4));
+    console.log(sum(2,4,8,9));
+    console.log(sum(20,45,89,91));
+    
     /*
     Declare uma variável chamada `userEntry`, que irá receber alguns valores
     entrados pelo usuário. Mostre para o usuário a seguinte frase:
     "Entre com alguns números que serão somados:"
     */
-    // ?
+    var userEntry = prompt('Entre com alguns números que serão somados:');
 
     /*
     Mostre no console o valor entrado pelo usuário:
     */
     console.log( '\nEntrada do usuário:' );
-    // ?
+    console.log(userEntry);
 
     /*
     Crie uma função chamada `justNumbers`, que recebe por parâmetro uma string
@@ -73,19 +76,27 @@
     da string. Mostre a representação em string dessa função no console.
     */
     console.log( '\nFunção que limpa entrada do usuário (somente números):' );
-    // ?
+    function justNumbers(receivedString) {
+        return receivedString.replace(/\D/g, '').split('').map(function(item) {
+            return Number(item);
+        }); 
+    }
+    
+    console.log(justNumbers.toString());
+    console.log(justNumbers('Pedro Álvares Cabral[A] (Belmonte, 1467 ou 1468 – Santarém, c. 1520) foi um fidalgo, comandante militar, navegador e explorador português'));
 
     /*
     Usando a função acima, faça a limpeza dos valores entrados pelo usuário,
     atribuindo o resultado à uma variável `numbers`.
     */
     console.log( '\nEntrada do usuário limpa. Somente números:' );
-    // ?
+    var numbers = justNumbers(userEntry);
+    console.log(numbers);
 
     /*
     Agora com o array de números, utilize a função `sum` para somar todos os
     números desse array e mostre o resultado no console.
     */
     console.log( '\nSomar números entrados pelo usuário:' );
-    // ?
+    console.log(sum.apply(numbers));
 })();
