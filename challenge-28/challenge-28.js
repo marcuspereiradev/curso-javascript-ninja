7ee21b018ebe9e45b284a93a1541f3568a8918ea
marcuspereiragithub <vinnycoast@gmail.com>
2018-06-28 14:54:09 -0300

    challenge-28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,4 +1,6 @@
-  /*
+(function() {
+    
+      /*
   No HTML:
   - Crie um formulário com um input de texto que receberá um CEP e um botão
   de submit;
@@ -25,3 +27,82 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+    'use strict';
+    function DOM(elements) {
+        this.element = document.querySelectorAll(elements);
+    }
+    
+    DOM.prototype.on = function on(eventType, callback) {
+        Array.prototype.forEach.call(this.element, function(element) {
+            element.addEventListener(eventType, callback, false);
+        });
+    };
+    
+    DOM.prototype.off = function on(eventType, callback) {
+        Array.prototype.forEach.call(this.element, function(element){
+            element.removeEventListener(eventType, callback, false);
+        });
+    };
+    
+    DOM.prototype.get = function get() {
+        return this.element;
+    };
+    
+    DOM.prototype.forEach = function forEach() {
+        Array.prototype.forEach.apply(this.element, arguments);
+    };
+    
+    DOM.prototype.map = function map() {
+        Array.prototype.map.apply(this.element, arguments);
+    };
+    
+    DOM.prototype.filter = function filter() {
+        Array.prototype.filter.apply(this.element, arguments);
+    };
+    
+    DOM.prototype.reduce = function reduce() {
+        Array.prototype.reduce.apply(this.element, arguments);
+    };
+    
+    DOM.prototype.reduceRight = function reduceRight() {
+        Array.prototype.reduceRight.apply(this.element, arguments);
+    };
+    
+    DOM.prototype.every = function every() {
+        Array.prototype.every.apply(this.element, arguments);
+    };
+    
+    DOM.prototype.some = function some() {
+        Array.prototype.some.apply(this.element, arguments);
+    };
+    
+    DOM.prototype.isArray = function isArray(param) {
+        return Object.prototype.toString.call(param) === '[object Array]';
+    };
+    
+    DOM.prototype.isObject = function isObject(param) {
+        return Object.prototype.toString.call(param) === '[object Object]';
+    };
+    
+    DOM.prototype.isFunction = function isFunction(param) {
+        return Object.prototype.toString.call(param) === '[object Function]';
+    };
+    
+    DOM.prototype.isNumber = function isNumber(param) {
+        return Object.prototype.toString.call(param) === '[object Number]';
+    };
+    
+    DOM.prototype.isString = function isString(param) {
+        return Object.prototype.toString.call(param) === '[object String]';
+    };
+    
+     DOM.prototype.isBoolean = function isBoolean(param) {
+        return Object.prototype.toString.call(param) === '[object Boolean]';
+    };
+    
+    DOM.prototype.isNull = function isNull(param) {
+        return Object.prototype.toString.call(param) === '[object Undefined]' 
+        || Object.prototype.toString.call(param) === '[object Null]';
+    };
+})();
