6303b4098bee0a9b3e1bf38462561e17072bff43
marcuspereiragithub <vinnycoast@gmail.com>
2018-07-04 12:53:06 -0300

    challenge-28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -105,4 +105,58 @@
         return Object.prototype.toString.call(param) === '[object Undefined]' 
         || Object.prototype.toString.call(param) === '[object Null]';
     };
+    
+    var $formCEP = new DOM('[data-js="form-cep"]');
+    var $inputCEP = new DOM('[data-js="input-cep"]');
+    $formCEP.on('submit', handleSubmitFormCEP);
+    var ajax = new XMLHttpRequest();
+    
+    function handleSubmitFormCEP(event) {
+        event.preventDefault();
+        var url = getUrl();
+        ajax.open('GET', url);
+        ajax.send();
+        ajax.addEventListener('readystatechange', handleReadyStateChange);
+    }
+    function getUrl() {
+        return 'https://cep.correiocontrol.com.br/[CEP]/.json'
+            .replace('[CEP]', $inputCEP.get()[0].value.replace(/\D/g, ''));
+    }
+    function handleReadyStateChange() {
+        if(isRequestOk()) {
+            fillCEPFields();
+        }
+        console.log('Carregando...');
+    }
+    function isRequestOk() {
+        return ajax.readyState === 4 && ajax.status === 200;
+    }
+    function fillCEPFields() {
+        var data = parseData();
+        if(!data)
+            return console.log('ERRO!');
+        var $logradouro = new DOM('[data-js="logradouro"]');
+        var $bairro = new DOM('[data-js="bairro"]');
+        var $estado = new DOM('[data-js="estado"]');
+        var $cidade = new DOM('[data-js="cidade"]');
+        var $cep = new DOM('[data-js="cep"]');
+        $logradouro.get()[0].textContent = data.logradouro;
+    }
+    function parseData() {
+        var result;
+        try {
+            result = JSON.parse(ajax.responseText);
+        }
+        catch(e) {
+            return null;
+        }
+        return result;
+    }
+    function getMessage(type) {
+        return {
+            loading: 'Buscando informações para o CEP [CEP]...',
+            ok: 'Endereço referente ao CEP [CEP]:',
+            error: 'Não encontramos o endereço para o CEP [CEP].'
+        }[type];
+    }
 })();
